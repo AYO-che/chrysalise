f31e53723d245984bb3b972e3b6af9a07d2da337
AYO-che <ayouche250@gmail.com>
2026-03-27 17:04:54 +0100

    your message here

--- server/prisma.config.js
+++ server/prisma.config.js
@@ -1,13 +1,13 @@
-
-import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
+  earlyAccess: true,
+  schema: "./prisma/schema.prisma",
+  migrate: {
+    async adapter() {
+      const { PrismaPg } = await import("@prisma/adapter-pg");
+      return new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    },
   },
 });
--- server/prismaClient.js
+++ server/prismaClient.js
@@ -1,4 +1,9 @@
 // server/prismaClient.js
-import { PrismaClient } from "./generated/prisma/client.js"; // correct path
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
 export default prisma;
